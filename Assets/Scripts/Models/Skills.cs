08cdf851d752870a849d72148b617a50959cf36c
daria-ogulchanskaya <55715903+daria-ogulchanskaya@users.noreply.github.com>
2020-03-02 17:14:24 +0200

    Updated Skills.

--- Assets/Scripts/Models/Skills.cs
+++ Assets/Scripts/Models/Skills.cs
@@ -3,13 +3,13 @@
     public struct Skills
     {
         public int Damage { get; private set; }
-        public int Defence { get; private set; }
+        public int Defense { get; private set; }
         public int Speed { get; private set; }
 
         public Skills(int damage, int defence, int speed)
         {
             Damage = damage;
-            Defence = defence;
+            Defense = defence;
             Speed = speed;
         }
     }
